a6516aac4ecf8e0745f7c83d0c9c63f32eba344a
pukolka46-ui <pukolka46@gmail.com>
2026-01-20 13:33:14 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,15 @@
+async function login() {
+  const res = await fetch("/api/login", {
+    method: "POST",
+    body: JSON.stringify({
+      login: login.value,
+      password: password.value
+    })
+  })
 
+  if (res.ok) {
+    location.href = "/dashboard.html"
+  } else {
+    alert("Неверные данные")
+  }
+}
