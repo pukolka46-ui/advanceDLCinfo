28341c6af8a171006f8ca25ca355eb949d9df792
pukolka46-ui <pukolka46@gmail.com>
2026-01-20 15:46:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+async function login() {
+  const email = email.value;
+  const password = password.value;
 
+  const r = await fetch("/api/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password })
+  });
+
+  if (r.ok) {
+    location.href = "dashboard.html";
+  } else {
+    alert("Access denied");
+  }
+}
